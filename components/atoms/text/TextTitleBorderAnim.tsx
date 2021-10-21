cfb9ff2516ca4d3b24945a12c355eac3c3202ec2
Dekguh <kadekteguhmahesa@gmail.com>
2021-10-21 12:01:31 +0800

    change font size title footer

--- components/atoms/text/TextTitleBorderAnim.tsx
+++ components/atoms/text/TextTitleBorderAnim.tsx
@@ -3,7 +3,7 @@ import { ITextTitleBorderAnim } from '../../utils/types'
 
 const TextTitleBorderAnim : React.FC<ITextTitleBorderAnim> = ({ classes, text, textBordered }) => {
     return (
-        <h2 className={`${classes} font-bold text-6xl sm:text-8xl md:text-5xl lg:text-7xl text-gray-800`}>
+        <h2 className={`${classes} font-bold text-4xl sm:text-8xl md:text-5xl lg:text-7xl text-gray-800`}>
             {text} <div className='text-bordered'>
                 <span className='text'>{textBordered}</span>
                 <span className='bordered-white-anim'></span>
--- components/utils/other/index.ts
+++ components/utils/other/index.ts
@@ -1,6 +1,5 @@
 export function calPercentOf(currentNum : number, totalNum : number, windowHeight : number) {
-    const currentReal = currentNum + windowHeight
-    const cal = Math.ceil((currentReal/totalNum) * 100)
-    console.log({ currentReal, totalNum, windowHeight, cal })
+    const cal = Math.ceil((currentNum/totalNum - currentNum) * 100)
+    console.log({ totalNum, windowHeight, cal })
     return cal
 }
