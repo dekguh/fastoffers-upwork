02806850ec7972b7c81f300caa1a287a7a18501d
Dekguh <kadekteguhmahesa@gmail.com>
2021-10-21 14:34:51 +0800

    create button flip for footer

--- components/atoms/control/ButtonFlipped.tsx
+++ components/atoms/control/ButtonFlipped.tsx
@@ -3,15 +3,27 @@ import { IButton } from '../../utils/types'
 
 const ButtonFlipped : React.FC<IButton> = ({ classes, text, onClick, icon : Icon }) => {
     return (
-    <div className=''>
-        <button className={`button-secondary flipped-button ${classes}`} onClick={onClick}>
-            <div className='flex flex-row flex-nowrap items-center'>
-                <span className='button-text font-bold'>{text}</span>
-                <span className='button-icon'>
-                    <i className='text-lg'><Icon /></i>
-                </span>
-            </div>
-        </button>
+    <div className="button button--flip text-left">
+        <div className="btn button__face">
+            <button className={`button-secondary ${classes}`} onClick={onClick}>
+                <div className='flex flex-row flex-nowrap items-center'>
+                    <span className='button-text font-bold'>{text}</span>
+                    <span className='button-icon'>
+                        <i className='text-lg'><Icon /></i>
+                    </span>
+                </div>
+            </button>
+        </div>
+        <div className="btn button__top">
+            <button className={`button-secondary ${classes}`} onClick={onClick}>
+                <div className='flex flex-row flex-nowrap items-center'>
+                    <span className='button-text font-bold'>{text}</span>
+                    <span className='button-icon'>
+                        <i className='text-lg'><Icon /></i>
+                    </span>
+                </div>
+            </button>
+        </div>
     </div>
     )
 }
