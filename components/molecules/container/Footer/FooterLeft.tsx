bd69c0c9a20feac139fe25bdf756100c423cc79b
Dekguh <kadekteguhmahesa@gmail.com>
2021-10-23 07:38:51 +0800

    add meta, update footer button

--- components/molecules/container/Footer/FooterLeft.tsx
+++ components/molecules/container/Footer/FooterLeft.tsx
@@ -3,6 +3,7 @@ import ButtonSecondary from '../../../atoms/control/ButtonSecondary'
 import TextTitleBorderAnim from '../../../atoms/text/TextTitleBorderAnim'
 import { BsArrowRightShort } from 'react-icons/bs'
 import ButtonFlipped from '../../../atoms/control/ButtonFlipped'
+import Link from 'next/link'
 
 const FooterLeft : React.FC = () => {
     return (
@@ -17,11 +18,15 @@ const FooterLeft : React.FC = () => {
                 <p className='text-white leading-relaxed my-5 text-center md:text-left'>draft offers faster than ever before</p>
 
                 <div className='text-center md:inline-block'>
-                    <ButtonFlipped
-                        text="CLICK HERE TO BEGIN"
-                        icon={BsArrowRightShort}
-                        classes='py-4 px-14 rounded-full text-lg'
-                    />
+                    <Link href='https://forms.ltd/register'>
+                        <a>
+                            <ButtonSecondary
+                                text="CLICK HERE TO BEGIN"
+                                icon={BsArrowRightShort}
+                                classes='py-4 px-14 rounded-full text-lg hover:bg-white hover:text-secondary'
+                            />
+                        </a>
+                    </Link>
 
                     <p className='text-xs mt-3 text-center'>
                         totally <span className='font-bold'>free for 30 days</span>. No credit card required.
