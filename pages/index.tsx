2dd3d08275556a6f4b09a0ef2cb7be979dca8831
Dekguh <kadekteguhmahesa@gmail.com>
2021-10-23 07:44:59 +0800

    update meta detail

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,11 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>FastOffers - draft full real estate offers in a minute</title>
+        <title>FastOffers - Draft Offers Faster</title>
         <script src='./chat.js' async></script>
-        <meta name="description" content="draft full real estate offers in a minute" />
+        <meta name="description" content="Draft Offers Faster" />
         <link rel="icon" href="./favicon.ico" />
-        <meta property="og:title" content="FastOffers - draft full real estate offers in a minute" />
+        <meta property="og:title" content="FastOffers - Draft Offers Faster" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://fastoffers.ca/" />
         <meta property="og:image" content="./SocialShare.png" />
